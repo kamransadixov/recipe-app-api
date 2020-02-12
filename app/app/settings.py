4fc414cadd3d06cd6441d82b5e394107e46d7bd4
kamransadixov <kamransadixov@mail.ru>
2020-02-12 20:48:34 +0400

    Config POSTGRESQL_DB

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6%m!ck7-#grlp8x&ff%r6s&%j9uk*@-id$blq=u!2@40@vk3cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
